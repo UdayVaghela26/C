e3d5409de9ecd98761526eddc4981933b48cf979
UdayVaghela26 <uday26.08.2005@gmail.com>
2024-08-13 17:19:35 +0530

    first commit

---
+++ Day 1/Q1.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+
+int main()
+{
+    printf("Name:Uday\nAge:20\nClg:Grace College");
+
+}
---
+++ Day 1/Q2.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+
+int main()
+{
+    printf("---------\n|\t|\nR\t|\nN\t|\nW\t|\n|\t|\n---------");
+
+}
